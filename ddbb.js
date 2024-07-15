445462183718666408270c4e8ec37e6a317409f2
Agush <a_clemente@live.com.ar>
2024-07-15 02:59:25 -0300

    ddbb actualizada

--- ddbb.js
+++ ddbb.js
@@ -1,9 +1,13 @@
 var mysql      = require('mysql2');
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  /* host     : 'localhost',
   user     : 'root',
   password : 'agush',
-  database : 'turismo'
+  database : 'turismo' */
+  host     : "beivyosjyljis0ocyp2p-mysql.services.clever-cloud.com",
+  user     : 'uet0p8hxobxcaqkd',
+  password : 'llmkUqfw0YAkjHlRCcj0',
+  database : 'beivyosjyljis0ocyp2p'
 });
  
 connection.connect();
